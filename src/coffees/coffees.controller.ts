0431ef1c4066d7a832a6b26ac8c627f736a788a1
zattaoui <03zaki95@gmail.com>
2022-04-20 16:21:09 +0200

    clean code

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -2,8 +2,14 @@ import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
+
+  @Get()
+  findAll() {
+    return 'This action return all coffees';
+  }
+
   @Get(':id')
-  findAll(@Param('id') id: string) {
+  findOne(@Param('id') id: string) {
     return `This action return #${id} coffees`;
   }
 }
